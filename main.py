cd27c93d7fc61001951421ccfc95fca588c920d7
bulutburhan <burhanbulut73@gmail.com>
2025-12-28 17:06:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from kivy.uix.label import Label
 from kivy.clock import Clock
 from kivy.utils import platform
 
-# Android kütüphanelerine erişim için hazırlık
+# Android'de kütüphaneleri yükle
 if platform == 'android':
     from jnius import autoclass
     PythonActivity = autoclass('org.kivy.android.PythonActivity')
@@ -15,42 +15,37 @@ if platform == 'android':
 class FocusApp(App):
     def build(self):
         self.timer_running = False
-        self.seconds = 1500  # 25 Dakika (Pomodoro)
+        self.seconds = 1500 
         
         layout = BoxLayout(orientation='vertical', padding=20, spacing=20)
-        
         self.label = Label(text="25:00", font_size='50sp')
         layout.add_widget(self.label)
         
         self.start_btn = Button(text="Odaklanmayı Başlat", on_press=self.toggle_timer, background_color=(0, 1, 0, 1))
         layout.add_widget(self.start_btn)
-        
         return layout
 
     def toggle_timer(self, instance):
         if not self.timer_running:
-            # Başlatılıyor
             self.timer_running = True
-            self.start_btn.text = "Bitir / Mola Ver"
+            self.start_btn.text = "Bitir"
             self.start_btn.background_color = (1, 0, 0, 1)
             Clock.schedule_interval(self.update_timer, 1)
-            self.set_dnd(True) # DND Modunu Aç
+            self.set_dnd(True)
         else:
-            # Durduruluyor
             self.timer_running = False
-            self.start_btn.text = "Odaklanmayı Başlat"
+            self.start_btn.text = "Başlat"
             self.start_btn.background_color = (0, 1, 0, 1)
             Clock.unschedule(self.update_timer)
-            self.seconds = 1500 # Sayacı sıfırla
+            self.seconds = 1500
             self.update_label()
-            self.set_dnd(False) # DND Modunu Kapat
+            self.set_dnd(False)
 
     def update_timer(self, dt):
         if self.seconds > 0:
             self.seconds -= 1
             self.update_label()
         else:
-            # Süre bittiğinde otomatik durdur
             self.toggle_timer(None)
 
     def update_label(self):
@@ -58,32 +53,17 @@ class FocusApp(App):
         self.label.text = f'{mins:02}:{secs:02}'
 
     def set_dnd(self, enable):
-        """
-        Rahatsız Etmeyin modunu açıp kapatan fonksiyon
-        """
         if platform == 'android':
             activity = PythonActivity.mActivity
             nm = activity.getSystemService(Context.NOTIFICATION_SERVICE)
-            
-            # İzin kontrolü
             if not nm.isNotificationPolicyAccessGranted():
-                # Kullanıcıyı izin sayfasına yönlendir
-                intent = autoclass('android.content.Intent')(
-                    "android.settings.NOTIFICATION_POLICY_ACCESS_SETTINGS"
-                )
+                intent = autoclass('android.content.Intent')("android.settings.NOTIFICATION_POLICY_ACCESS_SETTINGS")
                 activity.startActivity(intent)
                 return
-
             if enable:
-                # DND Aç (INTERRUPTION_FILTER_NONE = Tam Sessizlik)
                 nm.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_NONE)
             else:
-                # DND Kapat (INTERRUPTION_FILTER_ALL = Her şeye izin ver)
                 nm.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALL)
-        else:
-            # PC testi için
-            status = "AÇILDI" if enable else "KAPATILDI"
-            print(f"Sistem: Rahatsız Etmeyin Modu {status}")
 
 if __name__ == '__main__':
     FocusApp().run()
